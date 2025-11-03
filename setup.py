b2e29787f4783636c0eff856f64b83474f0a90b0
shrey <shreyasrpshreyas@gmail.com>
2025-11-03 23:44:12 +0530

    @@@

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import find_packages,setup
 setup(
     name='chatapp',
     version='0.0.1',
-    author='Shreyas R P',
-    author_email='shreyasrpshreyas@gmail.com',
+    # author_email intentionally omitted for public repo privacy
     install_requires=["openai","langchain","langchain-openai","python-dotenv"],
     packages=find_packages()
 )
